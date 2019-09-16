a162cc5a63b22ead0a384c1015a57cb9ec4cadf3
RemindYZ <yzhao14thu@163.com>
2019-09-16 16:22:20 +0800

    finish before debug, need add logger

--- agents/MA_MDDPG.py
+++ agents/MA_MDDPG.py
@@ -16,6 +16,11 @@ class MA_MDDPG():
                  actor_hyperparameters):
         self.env = env
         self.neighbor_map = env.neighbor_map
+
+        self.state_height = int(self.env.ild_length/self.env.ver_length)
+        # self.state_width = 6
+        self.phase_size = 4
+
         self.parameters = parameters
         self.encoder_hyperparameters = encoder_hyperparameters
         self.critic_hyperparameters = critic_hyperparameters
@@ -78,11 +83,11 @@ class MA_MDDPG():
             self.next_obs, self.rewards, self.dones, _ = self.env.step(self.actions)
             if len(self.replaybuffer) >= self.parameters["batch_size"]:
                 for _ in range(self.parameters["learning_step_per_session"]):
-                    states, actions, rewards, next_states, local_memorys, dones = self.replaybuffer.sample()
-                    self.critic_learn(states, actions, rewards, next_states, local_memorys, dones)
-                    self.actor_learn(states, local_memorys)
-            obs, actions, rewards, next_obs, memory = self._dict_to_numpy(self.obs, self.actions, self.rewards, self.next_obs, self.memory)
-            self.replaybuffer.add_experience(obs, actions, rewards, next_obs, memory, self.dones)
+                    states, phases, actions, rewards, next_states, next_phases, local_memorys, dones = self.replaybuffer.sample()
+                    self.critic_learn(states, phases, actions, rewards, next_states, next_phases, local_memorys, dones)
+                    self.actor_learn(states, phases, local_memorys)
+            states, phases, actions, rewards, n_states, n_phases, memory = self._dict_to_numpy(self.obs, self.actions, self.rewards, self.next_obs, self.memory)
+            self.replaybuffer.add_experience(states, phases, actions, rewards, n_states, n_phases, memory, self.dones)
             self.obs = self.next_obs
             self.global_step_number += 1
             if self.dones == True: break
@@ -115,22 +120,84 @@ class MA_MDDPG():
         self.memory = {actor.name:actor.get_local_memory() for actor in self.actors}
         return actions
     
-    def _dict_to_numpy(self, states, actions, rewards, next_states, memory):
+    def _dict_to_numpy(self, obs, actions, rewards, next_obs, memory):
+        states, phases = obs
+        n_states, n_phases = next_obs
         state_numpy = np.stack([states[a_n] for a_n in self.actor_names], axis=2).reshape(-1)
-        action_numpy = np.stack([actions[a_n] for a_n in self.actor_names], axis=2).reshape(-1)
-        reward_numpy = np.stack([rewards[a_n] for a_n in self.actor_names], axis=2).reshape(-1)
-        n_state_numpy = np.stack([next_states[a_n] for a_n in self.actor_names], axis=2).reshape(-1)
-        memory_numpy = np.stack([memory[a_n] for a_n in self.actor_names], axis=2).reshape(-1)
-        return state_numpy, action_numpy, reward_numpy, n_state_numpy, memory_numpy
+        phase_numpy = np.stack([phases[a_n] for a_n in self.actor_names], axis=1).reshape(-1)
+        action_numpy = np.stack([actions[a_n] for a_n in self.actor_names], axis=0).reshape(-1)
+        reward_numpy = np.stack([rewards[a_n] for a_n in self.actor_names], axis=0).reshape(-1)
+        n_state_numpy = np.stack([n_states[a_n] for a_n in self.actor_names], axis=2).reshape(-1)
+        n_phase_numpy = np.stack([n_phases[a_n] for a_n in self.actor_names], axis=1).reshape(-1)
+        memory_numpy = np.stack([memory[a_n] for a_n in self.actor_names], axis=1).reshape(-1)
+        return state_numpy, phase_numpy, action_numpy, reward_numpy, n_state_numpy, n_phase_numpy, memory_numpy
     
-    def critic_learn(self, states, actions, rewards, next_states, local_memorys, dones):
-        pass
+    def critic_learn(self, states, phases, actions, rewards, next_states, next_phases, local_memorys, dones):
+        loss = self.compute_loss(states, phases, actions, rewards, next_states, next_phases, local_memorys, dones)
+        self._optim_critic(loss)
+        tau = self.parameters["critic_tau"]
+        for actor in self.actors:
+            for f_model, t_model in zip(actor.critic.parameters(), actor.critic_target.parameters()):
+                t_model.data.copy_(tau*f_model.data+(1-tau)*t_model.data)
 
-    def actor_learn(self, states, local_memorys):
-        pass
+    def actor_learn(self, states, phases, local_memorys):
+        if self.dones:
+            #updata learning rate
+            pass
+        state = states.reshape(-1, self.state_height, self.encoder_hyperparameters["state_size"], self.parameters["n_inter"])
+        phase = phases.reshape(-1, self.phase_size, self.parameters["n_inter"])
+        local_memory = local_memorys.reshape(-1, self.parameters["dim_memory"], self.parameters["n_inter"])
+        neighbor_map = [[self.actor_names.index(neigh) for neigh in self.env.neighbor_map[ac_name]] 
+                        for ac_in, ac_name in enumerate(self.actor_names)]
+        neighbor_memory = [local_memory[:,:,ind] for ind in neighbor_map]
+        actions_pred = torch.cat([self.actors[ac_in].actor(state[:,:,:,ac_in], phase[:,:,ac_in], local_memory[:,:,ac_in], neighbor_memory[ac_in]) 
+                            for ac_in, _ in enumerate(self.actor_names)], dim=1).reshape(-1, self.parameters["n_inter"])
+        action_loss = [-self.actors[i].critic(state, phase, actions_pred).mean() for i in range(len(self.parameters["n_inter"]))]
+        self._optim_actor(action_loss)
+        tau = self.parameters["actor_tau"]
+        for actor in self.actors:
+            for f_model, t_model in zip(actor.actor.parameters(), actor.actor_target.parameters()):
+                t_model.data.copy_(tau*f_model.data+(1-tau)*t_model.data)
 
-    def compute_loss(self, states, actions, rewards, next_states, local_memorys, dones):
-        pass
+    def compute_loss(self, states, phases, actions, rewards, next_states, next_phases, local_memorys, dones):
+        state = states.reshape(-1, self.state_height, self.encoder_hyperparameters["state_size"], self.parameters["n_inter"])
+        phase = phases.reshape(-1, self.phase_size, self.parameters["n_inter"])
+        action = actions.reshape(-1, self.parameters["n_inter"])
+        reward = rewards.reshape(-1, self.parameters["n_inter"])
+        next_state = next_states.reshape(-1, self.state_height, self.encoder_hyperparameters["state_size"], self.parameters["n_inter"])
+        next_phase = next_phases.reshape(-1, self.phase_size, self.parameters["n_inter"])
+        local_memory = local_memorys.reshape(-1, self.parameters["dim_memory"], self.parameters["n_inter"])
+        neighbor_map = [[self.actor_names.index(neigh) for neigh in self.env.neighbor_map[ac_name]] 
+                        for ac_in, ac_name in enumerate(self.actor_names)]
+        neighbor_memory = [local_memory[:,:,ind] for ind in neighbor_map]
+        done = dones.reshape(-1,1)
+        with torch.no_grad():
+            actions_next = torch.cat([self.actors[ac_in].actor_target(next_state[:,:,:,ac_in], next_phase[:,:,ac_in], local_memory[:,:,ac_in], neighbor_memory[ac_in]) 
+                            for ac_in, _ in enumerate(self.actor_names)], dim=1).reshape(-1, self.parameters["n_inter"])
+            critic_targets_next = torch.cat([self.actors[ac_in].critic_target(next_state, next_phase, actions_next) 
+                                   for ac_in, _ in enumerate(self.actor_names)], dim=1).reshape(-1, self.parameters["n_inter"])
+        critic_targets = reward + self.parameters["gamma"] * critic_targets_next * (1.0 - done)
+        crititc_expected = torch.cat([self.actors[ac_in].critic(state, phase, action) 
+                                      for ac_in, _ in enumerate(self.actor_names)], dim=1).reshape(-1, self.parameters["n_inter"])
+        loss = [functional.mse_loss(crititc_expected[:,i], critic_targets[:,i]) for i in range(critic_targets.shape[-1])]
+        return loss
+    
+    def _optim_critic(self, loss, clipping_norm=None):
+        for actor_ind, actor in enumerate(self.actors):
+            actor.critic_optimizer.zero_grad()
+            loss[actor_ind].backward()
+            if clipping_norm is not None:
+                torch.nn.utils.clip_grad_norm_(actor.critic.parameters(), clipping_norm)
+            actor.critic_optimizer.step()
+    
+    def _optim_actor(self, loss, clipping_norm=None):
+        for actor_ind, actor in enumerate(self.actors):
+            actor.actor_optimizer.zero_grad()
+            loss[actor_ind].backward()
+            if clipping_norm is not None:
+                torch.nn.utils.clip_grad_norm_(actor.actor.parameters(), clipping_norm)
+            actor.actor_optimizer.step()
+
 
 
 
--- utilities/ReplayBuffer.py
+++ utilities/ReplayBuffer.py
@@ -7,8 +7,8 @@ class Replay_Buffer():
     def __init__(self, buffer_size, batch_size, seed = None):
         self.memory = deque(maxlen=buffer_size)
         self.batch_size = batch_size
-        self.experience = namedtuple("Experience", field_names=["state", "action", "reward",
-                                     "next_state", "local_memory", "done"])
+        self.experience = namedtuple("Experience", field_names=["state", "phase", "action", "reward",
+                                     "next_state", "next_phase", "local_memory", "done"])
         if seed:
             self.seed = random.seed(seed)
         else:
@@ -18,35 +18,37 @@ class Replay_Buffer():
     def __len__(self):
         return len(self.memory)
     
-    def add_experience(self, states, actions, rewards, next_states, local_memorys, dones):
+    def add_experience(self, states, phases, actions, rewards, next_states, next_phases, local_memorys, dones):
         if type(dones) == list:
             assert type(dones[0]) != list, "A done shouldn't be a list"
-            experiences = [self.experience(state, action, reward, next_state, local_memory, done)
-                           for state, action, reward, next_state, local_memory, done in
-                           zip(states, actions, rewards, next_states, local_memorys, dones)]
+            experiences = [self.experience(state, phase, action, reward, next_state, next_phase, local_memory, done)
+                           for state, phase, action, reward, next_state, next_phase, local_memory, done in
+                           zip(states, phases, actions, rewards, next_states, next_phases, local_memorys, dones)]
             self.memory.extend(experiences)
         else:
-            experience = self.experience(states, actions, rewards, next_states, local_memorys, dones)
+            experience = self.experience(states, phases, actions, rewards, next_states, next_phases, local_memorys, dones)
             self.memory.append(experience)
     
     def sample(self, num_experiences=None, separate_out_data_types=True):
         experiences = self.pick_experiences(num_experiences)
         if separate_out_data_types:
-            states, actions, rewards, next_states, local_memorys, dones = self.separate_out_data_types(experiences)
-            return states, actions, rewards, next_states, local_memorys, dones
+            states, phases, actions, rewards, next_states, next_phases, local_memorys, dones = self.separate_out_data_types(experiences)
+            return states, phases, actions, rewards, next_states, next_phases, local_memorys, dones
         else:
             return experiences
 
     def separate_out_data_types(self, experiences):
         """Puts the sampled experience into the correct format for a PyTorch neural network"""
         states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(self.device)
+        phases = torch.from_numpy(np.vstack([e.phase for e in experiences if e is not None])).float().to(self.device)
         actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).float().to(self.device)
         rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(self.device)
         next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(self.device)
+        next_phases = torch.from_numpy(np.vstack([e.next_phase for e in experiences if e is not None])).float().to(self.device)
         local_memorys = torch.from_numpy(np.vstack([e.local_memory for e in experiences if e is not None])).float().to(self.device)
         dones = torch.from_numpy(np.vstack([int(e.done) for e in experiences if e is not None])).float().to(self.device)
         
-        return states, actions, rewards, next_states, local_memorys, dones
+        return states, phases, actions, rewards, next_states, next_phases, local_memorys, dones
     
     def pick_experiences(self, num_experiences=None):
         if num_experiences is not None: batch_size = num_experiences
